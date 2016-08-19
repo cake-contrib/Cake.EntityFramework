2aa92f356e1daeee8bf6317efdd0e4d7a917e599
Mark Lopez <m@silvenga.com>
2016-08-18 22:50:39 -0500

    Added cake documentation, improved coverage on alias.

---
+++ src/Cake.EntityFramework6/CakeTranslation/Ef6Aliases.cs
@@ -0,0 +1,79 @@
+﻿namespace Cake.EntityFramework6.CakeTranslation
+{
+    using System;
+
+    using Cake.Core;
+    using Cake.Core.Annotations;
+    using Cake.EntityFramework6.Interfaces;
+    using Cake.EntityFramework6.Migrator;
+
+    /// <summary>
+    /// A set of Cake aliases for Entity Framework 6 code-first migrations.
+    /// </summary>
+    [CakeAliasCategory("EntityFramework6")]
+    public static class Ef6Aliases
+    {
+        /// <summary>
+        /// Creates a new EfMigrator for use in EF6 code-first migrations.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// using(var migrator = CreateEfMigrator(efMigratorSettings)) 
+        /// {
+        ///      migrator.MigrateToLatest();
+        ///      migrator.Commit();
+        /// }
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings <see cref="EfMigratorSettings"/>.</param>
+        /// <returns>An EfMigrator <see cref="IEfMigrator"/>.</returns>
+        [CakeMethodAlias]
+        public static IEfMigrator CreateEfMigrator(this ICakeContext context, EfMigratorSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            if (settings.AssemblyPath == null)
+            {
+                throw new ArgumentNullException(nameof(settings.AssemblyPath));
+            }
+
+            if (settings.AppConfigPath == null)
+            {
+                throw new ArgumentNullException(nameof(settings.AppConfigPath));
+            }
+
+            if (settings.ConfigurationClass == null)
+            {
+                throw new ArgumentNullException(nameof(settings.ConfigurationClass));
+            }
+
+            if (settings.ConnectionProvider == null)
+            {
+                throw new ArgumentNullException(nameof(settings.ConnectionProvider));
+            }
+
+            if (settings.ConnectionString == null)
+            {
+                throw new ArgumentNullException(nameof(settings.ConnectionString));
+            }
+
+            return new EfMigrator(
+                settings.AssemblyPath.FullPath,
+                settings.ConfigurationClass,
+                settings.AppConfigPath.FullPath,
+                settings.ConnectionString,
+                settings.ConnectionProvider,
+                new CakeLogger(context.Log)
+                );
+        }
+    }
+}
---
+++ tests/Cake.EntityFramework6.Tests/CakeAliases/Ef6AliasesFacts.cs
@@ -0,0 +1,82 @@
+﻿namespace Cake.EntityFramework6.Tests.CakeAliases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using Cake.Core;
+    using Cake.EntityFramework6.CakeTranslation;
+
+    using FluentAssertions;
+
+    using NSubstitute;
+
+    using Ploeh.AutoFixture;
+
+    using Xunit;
+
+    public class Ef6AliasesFacts
+    {
+        private static readonly Fixture AutoFixture = new Fixture();
+        private readonly ICakeContext _context;
+
+        public Ef6AliasesFacts()
+        {
+            _context = Substitute.For<ICakeContext>();
+        }
+
+        public static List<object[]> Expressions => new List<object[]>
+        {
+            new object[] {Expression(x => x.AppConfigPath)},
+            new object[] {Expression(x => x.AssemblyPath)},
+            new object[] {Expression(x => x.ConfigurationClass)},
+            new object[] {Expression(x => x.ConnectionProvider)},
+            new object[] {Expression(x => x.ConnectionString)},
+        };
+
+        [Theory, MemberData(nameof(Expressions))]
+        public void Empty_data_should_throw(Expression<Func<EfMigratorSettings, object>> expression)
+        {
+            var settings = AutoFixture.Build<EfMigratorSettings>()
+                                      .With(expression, null)
+                                      .Create();
+
+            // Act
+            Action action = () => _context.CreateEfMigrator(settings);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void Empty_settings_arg_throws()
+        {
+            EfMigratorSettings settings = null;
+
+            // Act
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action action = () => _context.CreateEfMigrator(settings);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void Empty_context_should_throw()
+        {
+            ICakeContext context = null;
+
+            // Act
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action action = () => context.CreateEfMigrator(null);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        private static Expression<Func<EfMigratorSettings, object>> Expression(Expression<Func<EfMigratorSettings, object>> expression)
+        {
+            return expression;
+        }
+    }
+}
