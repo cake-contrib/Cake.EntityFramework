bf8034606af3c9e5613fa3eea83c71c60e8e4ae4
Mark Lopez <m@silvenga.com>
2017-04-08 15:08:56 -0500

    Fixed build - working directory behavior of XUnit must have changed.

--- tests/Cake.EntityFramework6.Tests/Migrator/Postgres/PostgresFactConstants.cs
+++ tests/Cake.EntityFramework6.Tests/Migrator/Postgres/PostgresFactConstants.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace Cake.EntityFramework6.Tests.Migrator.Postgres
 {
     public static class PostgresFactConstants
     {
-        public const string DdlPath = @"Postgres\Cake.EntityFramework.TestProject.Postgres.dll";
+        public static readonly string DdlPath = $@"{AssemblyDirectory}\Postgres\Cake.EntityFramework.TestProject.Postgres.dll";
         public const string ConfigName = "Cake.EntityFramework.TestProject.Postgres.Migrations.Configuration";
-        public const string AppConfig = @"Postgres\Cake.EntityFramework.TestProject.Postgres.dll.config";
+        public static readonly string AppConfig = $@"{AssemblyDirectory}\Postgres\Cake.EntityFramework.TestProject.Postgres.dll.config";
 
         public static string InstanceConnectionString =>
             $"Host=127.0.0.1; Database=cake_dev_{Guid.NewGuid().ToString("N")}; Username=postgres; Password=Password12!;";
 
         public const string ConnectionProvider = "Npgsql";
+
+        // http://stackoverflow.com/a/283917
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
     }
 }
